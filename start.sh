e94b0214d0f262369568c4f98105e09fbab5d275
Allan <allangallop88@gmail.com>
2021-07-04 17:03:43 +0100

    First Commit

---
+++ start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+cd /etc/direwolf
+
+if [ -n "$CALLSIGN" -a  -n "$PASSCODE" ]; then
+  sed -i "s/^MYCALL.*$/MYCALL ${CALLSIGN}/g" direwolf.conf
+  sed -i "s/^IGLOGIN.*$/IGLOGIN ${CALLSIGN} ${PASSCODE}/g" direwolf.conf
+else
+  echo "CALLSIGN & PASSCODE are required."
+  exit 2
+fi
+
+## PBEACON line configuration
+if [ -n "$LATITUDE" -a -n "$LONGITUDE" ]; then
+  sed -i "s/%LATITUDE%/${LATITUDE}/g" direwolf.conf
+  sed -i "s/%LONGITUDE%/${LONGITUDE}/g" direwolf.conf
+  sed -i "s/%ALT%/${ALT}/g" direwolf.conf
+  sed -i "s/%SYMBOL%/${SYMBOL}/g" direwolf.conf
+  sed -i "s/%OVERLAY%/${OVERLAY}/g" direwolf.conf
+  sed -i "s~%COMMENT%~${COMMENT}~g" direwolf.conf
+else
+  echo "LATITUDE & LONGITUDE are required."
+  exit 3
+fi
+
+if [ -n "$ADEVICE" ]; then
+  sed -i "s/^ADEVICE.*$/ADEVICE ${ADEVICE//\//\\/}/g" direwolf.conf
+fi
+
+if [ -n "$IGSERVER" ]; then
+  sed -i "s/^IGSERVER.*$/IGSERVER ${IGSERVER}/g" direwolf.conf
+fi
+
+# Optionally start direwolf without rtl_fm as input
+if [ -n "$DW_STANDALONE" ]; then
+  if [ -n "$ADEVICE" ]; then
+    direwolf $DWARGS -c direwolf.conf
+  else
+    echo "DW_STANDALONE requires ADEVICE also be defined."
+    exit 4
+  fi
+else
+  direwolf $DWARGS -c direwolf.conf
+fi
